85e7b2c41c9ef1065c5f85207c415212febd556f
Pavel Gri <pavel.grigoryev@health.govt.nz>
2020-08-06 22:19:36 +0000

    Update test-pages.js

--- test/test-pages.js
+++ test/test-pages.js
@@ -3,7 +3,7 @@ var request = require('request');
 
 it('Main page content', function(done) {
     request('http://localhost:8080' , function(error, response, body) {
-        expect(body).to.equal('Hello World');
+        expect(body).to.include('Hello World');
         done();
     });
 });
