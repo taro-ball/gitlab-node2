013700e296f18bfbb3717b8629da7ec03215d122
Pavel Gri <pavel.grigoryev@health.govt.nz>
2020-08-10 04:18:29 +0000

    Update app/package-lock.json, app/app.js files

--- app/app.js
+++ app/app.js
@@ -1,15 +1,24 @@
-//Load express module with `require` directive
-var express = require('express')
-var app = express()
+var express = require('express');
+var http = require('http')
+var os = require('os')
+var app = express();
 
-//Define request response in root URL (/)
+const port = process.env.JSPORT || "8080";
+const head='<html><body bgcolor="#222530" text="#22CC22"><style>body { font-family: Arial, Helvetica, sans-serif; font-size: x-large;}</style><h1>Привет!你好！</h1> '
+const butt="</body></html>"
+system_info= 'hostname: '+os.hostname()+" / os: "+os.type()
+
+server = http.createServer(app).listen(port);
+var host = server.address().address;
+
 app.get('/', function (req, res) {
-  var fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
-  res.send('Hello Mates!\n'+fullUrl);
-  console.log('Received request for URL: ' + fullUrl);
-})
+  currentDate = new Date();
+  user_agent = req.get('user-agent')
+  ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+  content = `${currentDate} <br> ${ip} - ${user_agent}<br> `
+  res.send(`${head} ${content} <h2>${system_info}</h2> ${butt}`);
+  console.log(`${currentDate} - ${ip} - ${user_agent}`)
+});
 
-//Launch listening server on port 8080
-app.listen(8080, function () {
-  console.log('App listening on port 8080!')
-})
+console.log(system_info);
+console.log('Example app listening at http://%s:%s', host, port);
