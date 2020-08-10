72c3dad11fa5381f8020282a59111e32755d5e5f
Pavel Gri <pavel.grigoryev@health.govt.nz>
2020-08-10 22:38:00 +0000

    Revert "Update app/test/test-pages.js"
    This reverts commit 6bfbb054adfaf5e4b46835a90654f7d5ffca6127

---
+++ test/test-pages.js
@@ -0,0 +1,10 @@
+var expect  = require('chai').expect;
+var request = require('request');
+
+it('Main page content', function(done) {
+    request('http://localhost:8080' , function(error, response, body) {
+        expect(body).to.include('Hello World');
+        done();
+    });
+});
+
