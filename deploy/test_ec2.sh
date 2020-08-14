dba4270b2d8f3ec325bcb510b7f5e77f76b899fe
Pavel Gri <pavel.grigoryev@health.govt.nz>
2020-08-14 02:39:14 +0000

    Update .gitlab-ci.yml, deploy/test_ec2.sh files

--- deploy/test_ec2.sh
+++ deploy/test_ec2.sh
@@ -1,3 +1,4 @@
 
 #!/bin/bash
-sudo sh -c "echo shutdown -h now | at now + 5 minutes"; echo `date` > /tmp/01mydate.txt
+sudo sh -c "echo shutdown -h now | at now + 5 minutes"
+echo `date` > /tmp/01mydate.txt
