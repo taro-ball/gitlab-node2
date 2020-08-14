7c5c5d7d79f95295bf4523dac22be4d5c9ad3989
Pavel Gri <pavel.grigoryev@health.govt.nz>
2020-08-14 02:42:43 +0000

    Update deploy/test_ec2.sh

--- deploy/test_ec2.sh
+++ deploy/test_ec2.sh
@@ -1,4 +1,5 @@
 
 #!/bin/bash
-sudo sh -c "echo shutdown -h now | at now + 5 minutes"
+sudo sh -c "echo shutdown -h now | at now + 5 minutes" > /tmp/01sh_output.txt 2>&1
+sudo atq > /tmp/01atq.txt
 echo `date` > /tmp/01mydate.txt
