466b551c1e61a206a548bbaf1e2dfffba34c84a4
Pavel Gri <pavel.grigoryev@health.govt.nz>
2020-08-14 03:18:51 +0000

    Update deploy/test_ec2.sh, .gitlab-ci.yml files

--- deploy/test_ec2.sh
+++ deploy/test_ec2.sh
@@ -1,5 +1,5 @@
 
 #!/bin/bash
 sudo sh -c "echo shutdown -h now | at now + 5 minutes" > /tmp/01sh_output.txt 2>&1
-sudo atq > /tmp/01atq.txt
-echo `date` > /tmp/01mydate.txt
+sudo atq > /tmp/02atq.txt
+echo `date` > /tmp/03mydate.txt
