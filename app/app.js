3cfdcb877333b05c66fc685cc8b024654cb838a5
Pavel Gri <pavel.grigoryev@health.govt.nz>
2020-08-18 04:38:00 +0000

    Update app/app.js

--- app/app.js
+++ app/app.js
@@ -4,7 +4,7 @@ var os = require('os')
 var app = express();
 
 const port = process.env.JSPORT || "3000";
-const head='<html> <body bgcolor="#222530" text="#22CC22"><style>body { font-family: Arial, Helvetica, sans-serif; font-size: x-large;}</style><h1>Здравствуйте!你好！ Hello World!</h1> '
+const head='<html><body bgcolor="#222530" text="#22CC22"><style>body { font-family: Arial, Helvetica, sans-serif; font-size: x-large;}</style><h1>Здравствуйте!你好！ Hello World!</h1> '
 const butt= " <br><a href='/error'>try error</a><br><a href='/api/cpus'>try CPU api</a></body></html>"
 system_obj = [{"hostname":os.hostname(),"OS type":os.type()}]
 system_info = JSON.stringify(system_obj)
