6fdf47c3c5d972ecc082c965efd30e44acea764c
Pavel Gri <pavel.grigoryev@health.govt.nz>
2020-08-25 03:58:11 +0000

    Update .gitlab-ci.yml, app/app.js files

--- app/app.js
+++ app/app.js
@@ -9,8 +9,8 @@ const port = process.env.JSPORT || "3000";
 const head='<html><body bgcolor="#222530" text="#22CC22"><style>body { font-family: Arial, Helvetica, sans-serif; font-size: x-large;}</style><h1>Здравствуйте!你好！ Hello World!</h1> '
 const butt= " Hi Ben!!!!<br><a href='/error'>try error</a><br><a href='/api/cpus'>try CPU api</a><br><a href='/api/system'>try System api</a></body></html>"
 //system_obj = [{"hostname":os.hostname(),"OS type":os.type()}]
-system_obj = sysinfo.get()+sysinfo.resources();
-system_info = JSON.stringify(system_obj);
+system_obj = sysinfo.get()
+system_info = JSON.stringify(system_obj) +JSON.stringify(sysinfo.resources());
 server = http.createServer(app).listen(port);
 var host = server.address().address;
 
