777cf7cc4fb463a13fb75d3dd2afd963f6817d9d
EC2 Default User <ec2-user@ip-172-31-5-135.ap-southeast-2.compute.internal>
2020-09-24 22:47:48 +0000

    clean up dependencies

--- app1/test/my.test.js
+++ app1/test/my.test.js
@@ -1,9 +1,9 @@
 const assert = require('assert');
 describe('Simple Math Test', () => {
- it('should return 2', () => {
-        assert.equal(1 + 1, 3);
+ it('1 plus 1 is 2', () => {
+        assert.equal(1 + 1, 2);
     });
- it('should return 9', () => {
+ it('3 by 3 is 9', () => {
         assert.equal(3 * 3, 9);
     });
 });
--- app_old/sysinfo.js
+++
@@ -1,6 +0,0 @@
-var os = require('os')
-const sysinfo = {}; 
-sysinfo.get = () => [{"hostname":os.hostname(),"OS type":os.type()}];
-sysinfo.resources = () => [{"free mem":os.freemem(),"load avg":os.loadavg()}];
-sysinfo.cpus = () => os.cpus();
-module.exports = sysinfo;
